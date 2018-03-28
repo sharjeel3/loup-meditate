4f927a15fa1d86502fa6452852b895a58be4e30a
sharjeel3 <sharjeel.ahmad@bwired.com.au>
2018-03-28 22:04:14 +1100

    add card and section ui component

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -3,3 +3,5 @@ export { default as UIProvider } from './Provider/Provider'
 // components
 export { default as Button } from './Button/Button'
 export { default as PageBlock } from './PageBlock/PageBlock'
+export { default as Section } from './Section/Section'
+export { default as Card } from './Card/Card'
--- src/routes/activity/index.js
+++ src/routes/activity/index.js
@@ -1,13 +1,17 @@
 import { Component } from 'preact';
 import style from './style';
-import {PageBlock} from '../../components/UI'
+import {PageBlock, Card, Section} from '../../components/UI'
 
 export default class Activity extends Component {
     render() {
         return (
             <PageBlock>
-                <div class="">
-                    something comes here
+                <div class={style.root}>
+                    <Card>
+                        <Section>
+                            something comes here
+                        </Section>
+                    </Card>
                 </div>
             </PageBlock>
         );
